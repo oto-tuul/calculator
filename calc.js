cb17e1868027548b8b2e4d738cbfdeb4c9a8e97b
oto-tuul <oto.tuul@gmail.com>
2020-10-01 01:15:23 +0300

    Added consecutive calculations functionality

--- calc.js
+++ calc.js
@@ -1,18 +1,18 @@
 function add(num1, num2) {
     return num1 + num2;
-}
+};
 
 function subtract(num1, num2) {
     return num1 - num2;
-}
+};
 
 function multiply(num1, num2) {
     return num1 * num2;
-}
+};
 
 function divide(num1, num2) {
     return num1 / num2;
-}
+};
 
 function operate(operator, num1, num2) {
     if (operator == '+') {
@@ -27,79 +27,160 @@ function operate(operator, num1, num2) {
     if (operator == '/') {
         return divide(num1, num2);
     };
-}
+};
+
+function emptyDisplay() {
+    if (!executed) {
+        executed = true;
+        displayValue = '';
+    }
+};
 
 let displayValue = '';
 let num1;
 let num2;
 let operator
+let executed = false;
 
 document.querySelector('#addBtn').addEventListener('click', () => {
-    num1 = Number(displayValue);
-    operator = '+';
-    displayValue = '';
+    if (num1 != undefined) {
+        if (Number(displayValue) == 0) {
+            num2 = num2;
+        } else {
+            num2 = Number(displayValue)};
+        operator = '+';
+        result = operate(operator, num1, num2);
+        displayValue = result;
+        document.getElementById('display').innerHTML = displayValue;
+        num1 = result;
+        displayValue = '';
+        executed = false;
+    } else {
+        num1 = Number(displayValue);
+        operator = '+';
+        displayValue = '';
+        executed = false;
+        }
 });
 document.querySelector('#subtractBtn').addEventListener('click', () => {
-    num1 = Number(displayValue);
-    operator = '-';
-    displayValue = '';
+    if (num1 != undefined) {
+        if (Number(displayValue) == 0) {
+            num2 = num2;
+        } else {
+            num2 = Number(displayValue)};
+        operator = '-';
+        result = operate(operator, num1, num2);
+        displayValue = result;
+        document.getElementById('display').innerHTML = displayValue;
+        num1 = result;
+        displayValue = '';
+        executed = false;
+    } else {
+        num1 = Number(displayValue);
+        operator = '-';
+        displayValue = '';
+        executed = false;
+        }
 });
 document.querySelector('#multiplyBtn').addEventListener('click', () => {
-    num1 = Number(displayValue);
-    operator = '*';
-    displayValue = '';
+    if (num1 != undefined) {
+        if (Number(displayValue) == 0) {
+            num2 = num2;
+        } else {
+            num2 = Number(displayValue)};
+        operator = '*';
+        result = operate(operator, num1, num2);
+        displayValue = result;
+        document.getElementById('display').innerHTML = displayValue;
+        num1 = result;
+        displayValue = '';
+        executed = false;
+    } else {
+        num1 = Number(displayValue);
+        operator = '*';
+        displayValue = '';
+        executed = false;
+        }
 });
 document.querySelector('#divideBtn').addEventListener('click', () => {
-    num1 = Number(displayValue);
-    operator = '/';
-    displayValue = '';
+    if (num1 != undefined) {
+        if (Number(displayValue) == 0) {
+            num2 = num2;
+        } else {
+            num2 = Number(displayValue)};
+        operator = '/';
+        result = operate(operator, num1, num2);
+        displayValue = result;
+        document.getElementById('display').innerHTML = displayValue;
+        num1 = result;
+        displayValue = '';
+        executed = false;
+    } else {
+        num1 = Number(displayValue);
+        operator = '/';
+        displayValue = '';
+        executed = false;
+        }
 });
 document.querySelector('#resultBtn').addEventListener('click', () => {
     num2 = Number(displayValue);
     result = operate(operator, num1, num2);
     displayValue = result;
     document.getElementById('display').innerHTML = displayValue;
-    
+    executed = false;
 });
 
+document.querySelector('#numbers').addEventListener('click', () => {
+
+});
 
 document.querySelector('#oneBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '1';
     document.getElementById('display').innerHTML = displayValue;
 });
 document.querySelector('#twoBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '2';
     document.getElementById('display').innerHTML = displayValue;
 });
 document.querySelector('#threeBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '3';
     document.getElementById('display').innerHTML = displayValue;
 });
 document.querySelector('#fourBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '4';
     document.getElementById('display').innerHTML = displayValue;
 });
 document.querySelector('#fiveBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '5';
     document.getElementById('display').innerHTML = displayValue;
 });
 document.querySelector('#sixBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '6';
     document.getElementById('display').innerHTML = displayValue;
 });
 document.querySelector('#sevenBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '7';
     document.getElementById('display').innerHTML = displayValue;
 });
 document.querySelector('#eightBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '8';
     document.getElementById('display').innerHTML = displayValue;
 });
 document.querySelector('#nineBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '9';
     document.getElementById('display').innerHTML = displayValue;
 });
 document.querySelector('#zeroBtn').addEventListener('click', () => {
+    emptyDisplay ();
     displayValue += '0';
     document.getElementById('display').innerHTML = displayValue;
 });
@@ -109,17 +190,15 @@ document.querySelector('#dotBtn').addEventListener('click', () => {
     } else {
         displayValue += '.';
         document.getElementById('display').innerHTML = displayValue;
-        if (displayValue.includes('.')) {
-            document.getElementById('dotBtn').disabled = true;
-        }
     }
 });
 document.querySelector('#cBtn').addEventListener('click', () => {
     displayValue = '';
-    num1 = 0;
-    num2 = 0; 
+    num1 = undefined;
+    num2 = undefined; 
     document.getElementById('display').innerHTML = displayValue;
     document.getElementById('dotBtn').disabled = false;
+    executed = false;
 });
 document.querySelector('#backBtn').addEventListener('click', () => {
     displayValue = displayValue.slice(0, -1);
