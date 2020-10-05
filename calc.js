44f2d63a00fa489378ebd4aeb960c91cd0085612
oto-tuul <oto.tuul@gmail.com>
2020-10-05 22:29:48 +0300

    Fixed operating errors, javascript functionality complete

--- calc.js
+++ calc.js
@@ -42,15 +42,25 @@ let num2;
 let operator
 let executed = false;
 
+
 document.querySelector('#addBtn').addEventListener('click', () => {
     if (num1 != undefined) {
         if (Number(displayValue) == 0) {
-            num2 = num2;
+            operator = '+';
+            return;
         } else {
-            num2 = Number(displayValue)};
-        operator = '+';
+            num2 = Number(displayValue);
+        };
+
         result = operate(operator, num1, num2);
-        displayValue = result;
+        
+        if (result == Infinity || isNaN(result)) {
+            result = 'bruh.';
+        };
+
+        operator = '+';
+
+        displayValue = result.toString().substring(0, 9);
         document.getElementById('display').innerHTML = displayValue;
         num1 = result;
         displayValue = '';
@@ -58,19 +68,30 @@ document.querySelector('#addBtn').addEventListener('click', () => {
     } else {
         num1 = Number(displayValue);
         operator = '+';
-        displayValue = '';
+        
         executed = false;
         }
+    
+    document.getElementById('dotBtn').disabled = false;
 });
 document.querySelector('#subtractBtn').addEventListener('click', () => {
     if (num1 != undefined) {
         if (Number(displayValue) == 0) {
-            num2 = num2;
+            operator = '-';
+            return;
         } else {
-            num2 = Number(displayValue)};
-        operator = '-';
+            num2 = Number(displayValue);
+        };
+
         result = operate(operator, num1, num2);
-        displayValue = result;
+       
+        if (result == Infinity || isNaN(result)) {
+            result = 'bruh.';
+        };
+
+        operator = '-';
+
+        displayValue = result.toString().substring(0, 9);
         document.getElementById('display').innerHTML = displayValue;
         num1 = result;
         displayValue = '';
@@ -78,19 +99,29 @@ document.querySelector('#subtractBtn').addEventListener('click', () => {
     } else {
         num1 = Number(displayValue);
         operator = '-';
-        displayValue = '';
+        
         executed = false;
         }
+    document.getElementById('dotBtn').disabled = false;
 });
 document.querySelector('#multiplyBtn').addEventListener('click', () => {
     if (num1 != undefined) {
         if (Number(displayValue) == 0) {
-            num2 = num2;
+            operator = '*';
+            return;
         } else {
-            num2 = Number(displayValue)};
-        operator = '*';
+            num2 = Number(displayValue);
+        };
+
         result = operate(operator, num1, num2);
-        displayValue = result;
+    
+        if (result == Infinity || isNaN(result)) {
+            result = 'bruh.';
+        };
+
+        operator = '*';
+
+        displayValue = result.toString().substring(0, 9);
         document.getElementById('display').innerHTML = displayValue;
         num1 = result;
         displayValue = '';
@@ -101,16 +132,28 @@ document.querySelector('#multiplyBtn').addEventListener('click', () => {
         displayValue = '';
         executed = false;
         }
+    document.getElementById('dotBtn').disabled = false;
 });
 document.querySelector('#divideBtn').addEventListener('click', () => {
+    
+
     if (num1 != undefined) {
         if (Number(displayValue) == 0) {
-            num2 = num2;
+            operator = '/';
+            return;
         } else {
-            num2 = Number(displayValue)};
-        operator = '/';
+            num2 = Number(displayValue);
+        };
+
         result = operate(operator, num1, num2);
-        displayValue = result;
+
+        if (result == Infinity || isNaN(result)) {
+            result = 'bruh.';
+        };
+
+        operator = '/';
+
+        displayValue = result.toString().substring(0, 9);
         document.getElementById('display').innerHTML = displayValue;
         num1 = result;
         displayValue = '';
@@ -121,12 +164,22 @@ document.querySelector('#divideBtn').addEventListener('click', () => {
         displayValue = '';
         executed = false;
         }
+    document.getElementById('dotBtn').disabled = false;
 });
 document.querySelector('#resultBtn').addEventListener('click', () => {
     num2 = Number(displayValue);
+    if (num1 == undefined || num2 == undefined || operator == undefined) {
+        return;
+    }
     result = operate(operator, num1, num2);
-    displayValue = result;
+    if (result == Infinity || isNaN(result)) {
+        result = 'bruh.';
+    };
+    displayValue = result.toString().substring(0, 9);
     document.getElementById('display').innerHTML = displayValue;
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
     executed = false;
 });
 
@@ -137,55 +190,55 @@ document.querySelector('#numbers').addEventListener('click', () => {
 document.querySelector('#oneBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '1';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#twoBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '2';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#threeBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '3';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#fourBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '4';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#fiveBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '5';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#sixBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '6';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#sevenBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '7';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#eightBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '8';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#nineBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '9';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#zeroBtn').addEventListener('click', () => {
     emptyDisplay ();
     displayValue += '0';
-    document.getElementById('display').innerHTML = displayValue;
+    document.getElementById('display').innerHTML = displayValue.substring(0, 9);
 });
 document.querySelector('#dotBtn').addEventListener('click', () => {
-    if (!(Number.isInteger(displayValue))) {
+    if (document.getElementById('display').textContent.includes('.')) {
         document.getElementById('dotBtn').disabled = true;
     } else {
         displayValue += '.';
@@ -199,6 +252,11 @@ document.querySelector('#cBtn').addEventListener('click', () => {
     document.getElementById('display').innerHTML = displayValue;
     document.getElementById('dotBtn').disabled = false;
     executed = false;
+
+    addExecuted = false;
+    subtractExecuted = false;
+    multiplyExecuted = false;
+    divideExecuted = false;
 });
 document.querySelector('#backBtn').addEventListener('click', () => {
     displayValue = displayValue.slice(0, -1);
